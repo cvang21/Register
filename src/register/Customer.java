a76de0d1d5fa52f39eb7ba3b4e9556786385b623
Owner <Owner@192.168.1.111>
2014-09-29 16:50:57 -0500

    Firstl Commit

---
+++ src/register/Customer.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package register;
+
+/**
+ *
+ * @author Owner
+ */
+public class Customer {
+    private String customerLastName;
+    private String customerFirstName;
+    private String customerPhone;
+    private String customerCreditCard;
+    private String customerAccountNum;
+    private boolean customerRewardsActive;
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getCustomerCreditCard() {
+        return customerCreditCard;
+    }
+
+    public void setCustomerCreditCard(String customerCreditCard) {
+        this.customerCreditCard = customerCreditCard;
+    }
+
+    public String getCustomerAccountNum() {
+        return customerAccountNum;
+    }
+
+    public void setCustomerAccountNum(String customerAccountNum) {
+        this.customerAccountNum = customerAccountNum;
+    }
+
+    public boolean isCustomerRewardsActive() {
+        return customerRewardsActive;
+    }
+
+    public void setCustomerRewardsActive(boolean customerRewardsActive) {
+        this.customerRewardsActive = customerRewardsActive;
+    }
+    
+    
+}
---
+++ src/register/Store.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package register;
+
+/**
+ *
+ * @author Owner
+ */
+public class Store {
+    
+    private String storeNum;
+    private String storeName;
+    private String storeStreetAddress;
+    private String storeCity;
+    private String storeState;
+    private String storePhone;
+
+    public String getStoreNum() {
+        return storeNum;
+    }
+
+    public void setStoreNum(String storeNum) {
+        this.storeNum = storeNum;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStoreStreetAddress() {
+        return storeStreetAddress;
+    }
+
+    public void setStoreStreetAddress(String storeStreetAddress) {
+        this.storeStreetAddress = storeStreetAddress;
+    }
+
+    public String getStoreCity() {
+        return storeCity;
+    }
+
+    public void setStoreCity(String storeCity) {
+        this.storeCity = storeCity;
+    }
+
+    public String getStoreState() {
+        return storeState;
+    }
+
+    public void setStoreState(String storeState) {
+        this.storeState = storeState;
+    }
+
+    public String getStorePhone() {
+        return storePhone;
+    }
+
+    public void setStorePhone(String storePhone) {
+        this.storePhone = storePhone;
+    }
+    
+    
+    
+}
